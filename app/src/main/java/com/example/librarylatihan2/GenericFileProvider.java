048c180a9ee7569b1d63d3a8104062b367344014
Ikhwan Atoilah <ikhwanatoilah413@gmail.com>
2019-09-16 13:45:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/librarylatihan2/GenericFileProvider.java
@@ -0,0 +1,7 @@
+package com.example.librarylatihan2;
+
+import android.content.ContentProvider;
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider { }
